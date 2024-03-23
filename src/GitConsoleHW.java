81933b6dc8eeef106ad7c58bad5b63ca7ed901a0
ArcBLR <arc11041977@gmail.com>
2024-03-23 09:16:05 +0300

    add code

--- src/GitConsoleHW.java
+++ src/GitConsoleHW.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class GitConsoleHW {
+public class GitConsoleHW {
+    public static void main(String[] args) {
+        System.out.println("Создание нового проекта");
+        System.out.println("создал новую ветку и переключился на неё");
+        System.out.println("немного добавил код т закомитился в develop");
+        System.out.println("немного добавил код т закомитился в develop");
+    }
 }
